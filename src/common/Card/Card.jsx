36cab7c04df1d6d0a1668ecb52327f87693ae01f
Alexandr <sshnipov@gmail.com>
2022-08-30 14:21:38 +0400

    fixed Card.module.scss

--- src/common/Card/Card.jsx
+++ src/common/Card/Card.jsx
@@ -6,13 +6,15 @@ import {withRouter} from "../../hocs/withRouter";
 
 class Card extends Component {
   render() {
-   // console.log(this.props.location)
-    return(
- <NavLink className={s.card} to={`${this.props.location.pathname}/productCard`}>
-   <img className={s.cardImg} src={this.props.image} alt={this.props.name}/>
-   <h2 className={s.cardTitle}>{this.props.brand}, <span>{this.props.name}</span></h2>
-   <p className={s.cardPrice}><span>{this.props.currency}</span> {this.props.price}</p>
- </NavLink>
+    // console.log(this.props.location)
+    return (
+      <NavLink className={s.card} to={`${this.props.location.pathname}/productCard`}>
+        <div className={s.cardImg}>
+          <img src={this.props.image} alt={this.props.name}/>
+        </div>
+        <h2 className={s.cardTitle}>{this.props.brand}, <span>{this.props.name}</span></h2>
+        <p className={s.cardPrice}><span>{this.props.currency}</span> {this.props.price}</p>
+      </NavLink>
 
     )
   }
