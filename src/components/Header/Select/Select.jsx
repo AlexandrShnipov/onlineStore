8b8a5d32782e3ba26d68d2aff5c8f87238853e78
Alexandr <sshnipov@gmail.com>
2022-09-19 23:47:52 +0400

    fixed Select.jsx

--- src/components/Header/Select/Select.jsx
+++ src/components/Header/Select/Select.jsx
@@ -5,11 +5,14 @@ import {gql, request} from "graphql-request";
 import {connect} from "react-redux";
 import {setCurrencyAC} from "../../../redux/cartReducer";
 import {selectCurrencyLabel} from "../../../redux/catrSelectors";
+import arrow from '../../../images/arrowForSelect.png'
 
 class Select extends Component {
 
   state = {
-    currencies: []
+    currencies: [],
+    selectedCurrency: 0,
+    isActive: false
   }
 
   componentDidMount() {
@@ -33,27 +36,73 @@ class Select extends Component {
       .catch(err => console.log(err))
   }
 
-  onChange = (event) => {
-    const { currencies } = this.state;
+  //
+  // onChange = (event) => {
+  //   const {currencies} = this.state;
+  //   const selectedCurrency = currencies.find(cur => cur.label === event.target.value)
+  //   this.props.setCurrencyAC(selectedCurrency)
+  //   console.log(event.target.value)
+  // }
+
+  getSelectedCurrency = (index,) => (event) => {
+    this.setState({
+      selectedCurrency: index,
+      isActive: false
+    })
+    const {currencies} = this.state;
     const selectedCurrency = currencies.find(cur => cur.label === event.target.value)
     this.props.setCurrencyAC(selectedCurrency)
-    console.log(event.target.value)
   }
 
+  inputHandlerClickForOpenDropdown = () => {
+    this.setState({
+      isActive: true
+    })
+  }
+
+
   render() {
-    const {state: { currencies }, props: {currency}} = this;
+    const {state: {currencies}, props: {currency}} = this;
+    console.log(currencies)
     return (
-      <select className={s.select} value={currency} onChange={this.onChange}>
-        {currencies.map((item, index) =>
-          <option key={index} value={item.label}>
-            {item.symbol} {item.label}
-          </option>)}
-      </select>
+      <>
+        {/*<select className={s.select} onChange={this.onChange}>*/}
+        {/*  {currencies.map((item, index) =>*/}
+        {/*    <option key={index} value={item.label}>*/}
+        {/*      {item.symbol} {item.label}*/}
+        {/*    </option>)}*/}
+        {/*</select>*/}
+
+        <div className={s.select}>
+          <input className={s.selectBtn}
+                 value={currencies[this.state.selectedCurrency]?.symbol}
+                 onClick={this.inputHandlerClickForOpenDropdown}
+            // onChange={this.onChange}
+          />
+          {!this.state.isActive ?
+            <img className={s.arrow} src={arrow} alt={arrow} style={{transform: 'rotate(180deg)'}}/>
+            : <img className={s.arrow} src={arrow} alt={arrow} style={{transform: 'rotate(0)'}}/>
+          }
+          {this.state.isActive &&
+          <ul className={s.selectContent}>
+            {currencies.map((item, index) =>
+              <li className={s.selectItemWrap}>
+                {item.label} {item.symbol}
+                <input className={s.selectItem}
+                       key={index}
+                       value={item.label}
+                       onClick={this.getSelectedCurrency(index)}/>
+              </li>
+            )}
+          </ul>
+          }
+        </div>
+      </>
     )
   }
 }
 
 export default connect(
-  state => ({ currency: selectCurrencyLabel(state) }),
-{setCurrencyAC})
+  state => ({currency: selectCurrencyLabel(state)}),
+  {setCurrencyAC})
 (withRouter(Select));
